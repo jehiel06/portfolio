3dd63332619768a111a784ed4ee7b4611db12882
jehiel06 <jehiel06.dev@gmail.com>
2026-01-04 15:47:55 +0530

    Index

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePluginSitemap } from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+  VitePluginSitemap({
+    hostname: 'https://your-portfolio.vercel.app',
+    dynamicRoutes: [
+      '/',
+      '/#about',
+      '/#projects',
+      '/#contact'
+    ],
+  })
+  ],
 })
